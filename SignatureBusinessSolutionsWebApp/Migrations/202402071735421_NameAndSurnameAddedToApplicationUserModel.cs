1e0700f3c756162e0bd899cfac6ac17da2abba09
Assassin007120 <sooklallashton@gmail.com>
2024-02-07 19:36:20 +0200

    - Added Name and Surname to applicationUser - Updated dB

---
+++ SignatureBusinessSolutionsWebApp/Migrations/202402071735421_NameAndSurnameAddedToApplicationUserModel.cs
@@ -0,0 +1,16 @@
+﻿namespace SignatureBusinessSolutionsWebApp.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class NameAndSurnameAddedToApplicationUserModel : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
