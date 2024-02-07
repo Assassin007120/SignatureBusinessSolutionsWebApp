1a7f4c62e831bafecd812a284b12f324b25b76ec
Assassin007120 <sooklallashton@gmail.com>
2024-02-07 19:20:12 +0200

    - Migrations Enabled - InitialDb created

---
+++ SignatureBusinessSolutionsWebApp/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace SignatureBusinessSolutionsWebApp.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<SignatureBusinessSolutionsWebApp.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(SignatureBusinessSolutionsWebApp.Models.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
